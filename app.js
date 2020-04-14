f0907d9c9ddcf29c01032286220729b5fb78f8c0
whatcodehaveyougit <siggywatt@yahoo.com>
2020-04-14 15:11:51 +0100

    test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    app.get('/', (req, res) => res.send('Hello World!'))
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
