432d715887c137c854b1fe89880decac16884f06
whatcodehaveyougit <siggywatt@yahoo.com>
2020-04-14 15:58:38 +0100

    test

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 var path = require("path");
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.get('/', function (req, res) {
-    app.get('/', (req, res) => res.send('Hello World!'))
-});
+app.get('/', (req, res) => res.send('Hello World!'))
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 console.log(`${process.env.AWS_ACCESS_KEY_ID}`)
+
