90dcd576acf56b68bc6584df6a4e4f94d839ed5f
whatcodehaveyougit <siggywatt@yahoo.com>
2020-04-14 16:01:41 +0100

    test

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ var path = require("path");
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 console.log(`${process.env.AWS_ACCESS_KEY_ID}`)
+
+
 
